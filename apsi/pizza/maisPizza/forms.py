a9834cbbcac3d88f59da5d4046b880fd84681320
berg45 <pereiraberg62@gmail.com>
2024-08-27 09:39:07 -0300

    novas adições

--- apsi/pizza/maisPizza/forms.py
+++ apsi/pizza/maisPizza/forms.py
@@ -4,7 +4,15 @@ from .models import *
 class FormularioCliente(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = "__all__"
+        fields = ['nome', 'telefone', 'cep', 'endereco', 'bairro']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'telefone': forms.TextInput(attrs={'class': 'form-control'}),
+            'cep': forms.TextInput(attrs={'class': 'form-control'}),
+            'endereco': forms.TextInput(attrs={'class': 'form-control'}),
+            'bairro': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
     
 class FormularioPedido(forms.ModelForm):
     class Meta:
